f66b141c423ab37f32e4dfd2ddc5c6158aac15ac
DoSomehow <329237665@qq.com>
2019-05-29 01:10:31 +0800

    create project

---
+++ src/main/java/org/ms/wetalk/WetalkApplication.java
@@ -0,0 +1,13 @@
+package org.ms.wetalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WetalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WetalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ms/wetalk/WetalkApplicationTests.java
@@ -0,0 +1,16 @@
+package org.ms.wetalk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WetalkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
