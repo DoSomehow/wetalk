b114ea8ad40310616b8ac85a911fc54f429050dd
DoSomehow <329237665@qq.com>
2019-05-29 12:42:04 +0800

    test upload

--- src/main/java/org/ms/wetalk/WetalkApplication.java
+++ src/main/java/org/ms/wetalk/WetalkApplication.java
@@ -2,12 +2,18 @@ package org.ms.wetalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class WetalkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WetalkApplication.class, args);
+	}
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello world";
 	}
 
 }
